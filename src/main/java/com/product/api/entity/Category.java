9313563f066659c6db6b83b5c4bf633f667a5275
Supr-Lilito <lilito@ciencias.unam.mx>
2025-09-08 23:04:14 -0600

    Quickfix : pom.xml & minor adjustments

--- src/main/java/com/product/api/entity/Category.java
+++ src/main/java/com/product/api/entity/Category.java
@@ -3,12 +3,10 @@ package com.product.api.entity;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 
-import jakarta.persistence.Column;
+
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
