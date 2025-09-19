34549ce1cb990ca346d3e420f9cfc28f15dfc142
Supr-Lilito <lilito@ciencias.unam.mx>
2025-09-18 19:54:34 -0600

    feat: exceptions added

--- src/main/java/com/product/api/controller/CtrlCategory.java
+++ src/main/java/com/product/api/controller/CtrlCategory.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +20,8 @@ public class CtrlCategory {
 	SvcCategory svc;
 
     @GetMapping
-    public List<Category> getCategories() {
+    public ResponseEntity<List<Category>> getCategories() {
     	
-        return svc.getCategories();
+        return  svc.getCategories();
     }
 }
--- src/main/java/com/product/api/service/SvcCategoryImp.java
+++ src/main/java/com/product/api/service/SvcCategoryImp.java
@@ -3,6 +3,8 @@ package com.product.api.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.product.api.entity.Category;
@@ -15,8 +17,10 @@ public class SvcCategoryImp implements SvcCategory{
 	RepoCategory repo;
 
 	@Override
-	public List<Category> getCategories() {
-		return repo.getCategories();
-	}
+    public ResponseEntity<List<Category>> getCategories() {
+        return new ResponseEntity<>(repo.getCategories(), HttpStatus.OK);
+    }
+	
+	
 
 }
