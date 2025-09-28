64fb88656197e28e57ad7f0d975e5159fbc9c5d9
Supr-Lilito <lilito@ciencias.unam.mx>
2025-09-28 16:03:05 -0600

    refactor: Exception in SvcCategoryImp

--- src/main/java/com/product/api/service/SvcCategoryImp.java
+++ src/main/java/com/product/api/service/SvcCategoryImp.java
@@ -3,12 +3,14 @@ package com.product.api.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.product.api.entity.Category;
 import com.product.api.repository.RepoCategory;
+import com.product.exception.ApiException;
 
 @Service
 public class SvcCategoryImp implements SvcCategory{
@@ -18,7 +20,12 @@ public class SvcCategoryImp implements SvcCategory{
 
 	@Override
     public ResponseEntity<List<Category>> getCategories() {
-        return new ResponseEntity<>(repo.getCategories(), HttpStatus.OK);
+		try {
+			return new ResponseEntity<>(repo.getCategories(), HttpStatus.OK);
+		} catch (DataAccessException e) {
+			System.out.println(e.getLocalizedMessage());
+	        throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, "Error al consultar la base de datos");
+		}
     }
 	
 	
